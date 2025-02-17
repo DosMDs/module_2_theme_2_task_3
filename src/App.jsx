8bb0c01fd6e8df04f7d6974478c5b2cf67049f0c
Oleg Skakunov <dosmds@gmail.com>
2025-02-17 22:07:19 +0300

    добавил стейты и базовый вывод кнопок

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
 import "./App.css";
+import data from "./data.json";
 
 function App() {
-  return <></>;
+  const [buttons, setButtons] = useState(data);
+  const [operation, setOperation] = useState("");
+  const [operand1, setOperand1] = useState(0);
+  const [operand2, setOperand2] = useState(0);
+
+  const onButtonClick = (event) => {
+    console.log(event);
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="input">Ввод</label>
+        <input type="number" name="input" />
+        <div className="button-container" onClick={onButtonClick}>
+          {buttons.map((button) => {
+            return <button key={button}>{button}</button>;
+          })}
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default App;
