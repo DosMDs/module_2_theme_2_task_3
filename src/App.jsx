bacd4cbab00a4e5912fca3ccce775e9e718ce7c2
Oleg Skakunov <dosmds@gmail.com>
2025-02-18 09:34:17 +0300

    изменил формат данных

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,35 @@ import "./App.css";
 import data from "./data.json";
 
 function App() {
-  const [buttons, setButtons] = useState(data);
-  const [operation, setOperation] = useState("");
-  const [operand1, setOperand1] = useState(0);
-  const [operand2, setOperand2] = useState(0);
+	const [buttons, setButtons] = useState(data);
+	const [operation, setOperation] = useState("");
+	const [operand1, setOperand1] = useState(0);
+	const [operand2, setOperand2] = useState(0);
 
-  const onButtonClick = (event) => {
-    console.log(event);
-  };
+	const onButtonClick = (event) => {
+		console.log(event);
+	};
 
-  return (
-    <>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="input">Ввод</label>
-        <input type="number" name="input" />
-        <div className="button-container" onClick={onButtonClick}>
-          {buttons.map((button) => {
-            return <button key={button}>{button}</button>;
-          })}
-        </div>
-      </form>
-    </>
-  );
+	return (
+		<>
+			<form onSubmit={(e) => e.preventDefault()}>
+				<label htmlFor="input">Ввод</label>
+				<input type="number" name="input" />
+				<div className="button-container" onClick={onButtonClick}>
+					{buttons.map((button) => {
+						return (
+							<button
+								key={button.id}
+								data-operator={button.isOperator}
+							>
+								{button.value}
+							</button>
+						);
+					})}
+				</div>
+			</form>
+		</>
+	);
 }
 
 export default App;
