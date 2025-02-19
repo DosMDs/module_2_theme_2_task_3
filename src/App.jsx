13e70f414b0a91152c625041a07680e55e2faddc
Oleg Skakunov <dosmds@gmail.com>
2025-02-19 12:53:05 +0300

    реализовал операции

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,51 @@ import "./App.css";
 import data from "./data.json";
 
 function App() {
-	const [buttons, setButtons] = useState(data);
+	const NUMS = data;
 	const [operation, setOperation] = useState("");
 	const [operand1, setOperand1] = useState("");
 	const [operand2, setOperand2] = useState("");
 
+	const executeOperation = (currentOperation) => {
+		if (!operand1 && currentOperation !== "=") {
+			setOperation(currentOperation);
+			return;
+		}
+
+		if (currentOperation === "C") {
+			setOperand1("");
+			setOperand2("");
+			setOperation("");
+			return;
+		}
+
+		if (operation) {
+			switch (operation) {
+				case "+":
+					setOperand2(String(Number(operand2) + Number(operand1)));
+					break;
+				case "-":
+					setOperand2(String(Number(operand2) - Number(operand1)));
+					break;
+				default:
+					break;
+			}
+		} else {
+			setOperand2(operand1);
+		}
+
+		if (currentOperation === "=") {
+			setOperation("");
+		} else {
+			setOperation(currentOperation);
+		}
+		setOperand1("");
+	};
+
 	useEffect(() => {
 		const handleKeyUp = (event) => {
 			if ("+-=".indexOf(event.key) >= 0) {
-				setOperand2(operand1);
-				setOperand1("");
-				setOperation(event.key);
+				executeOperation(event.key);
 			} else if ("1234567890".indexOf(event.key) >= 0) {
 				setOperand1(operand1 + event.key);
 			}
@@ -26,12 +60,9 @@ function App() {
 	});
 
 	const onButtonClick = (event) => {
-		console.log(event);
 		if (event.target.tagName === "BUTTON") {
 			if (isNaN(event.target.dataset.value)) {
-				setOperand2(operand1);
-				setOperand1("");
-				setOperation(event.target.dataset.value);
+				executeOperation(event.target.dataset.value);
 			} else {
 				setOperand1(operand1 + event.target.dataset.value);
 			}
@@ -40,13 +71,14 @@ function App() {
 
 	return (
 		<>
-			<output>{operand2 + " " + operation}</output>
-			<output>{operand1 || "0"}</output>
+			<output>
+				{operand2 + " " + operation + " " + operand1 || "0"}
+			</output>
 			<div className="button-container" onClick={onButtonClick}>
-				{buttons.map((button) => {
+				{NUMS.map((num) => {
 					return (
-						<button key={button.id} data-value={button.value}>
-							{button.value}
+						<button key={num.id} data-value={num.value}>
+							{num.value}
 						</button>
 					);
 				})}
